747275630bed0ec5331f4b6e9718b16196938de9
issaahr <isabeli.romano@gmail.com>
2024-10-30 23:58:02 -0300

    feat: adicionar `validationMiddleware` ao `StudentRoutes`

--- src/routes/StudentRoutes.ts
+++ src/routes/StudentRoutes.ts
@@ -2,9 +2,11 @@ import { Router } from 'express';
 import { Container } from 'typedi';
 import { StudentController } from '../controller/StudentController';
 import { authMiddleware } from '../middlewares/authMiddleware';
+import { validationMiddleware } from '../middlewares/validateMiddleware';
+import { CreateStudentDTO } from '../models/DTO/CreateStudentDTO';
 
 const studentRoutes = Router();
 const studentController: StudentController = Container.get(StudentController);
-studentRoutes.post('/register', authMiddleware, (req, res) => studentController.create(req, res));
+studentRoutes.post('/register', authMiddleware, validationMiddleware(CreateStudentDTO), (req, res) => studentController.create(req, res));
 
 export default studentRoutes;
