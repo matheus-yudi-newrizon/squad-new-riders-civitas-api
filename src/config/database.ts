64d0112eaadceb1105341993eab6e22c1a22987f
rgbritoamanda <amandaramires.brito@gmail.com>
2024-11-08 10:51:38 -0300

    feat: adiciona .dockerignore para otimizar imagem Docker

--- src/config/database.ts
+++ src/config/database.ts
@@ -9,8 +9,5 @@ export const MysqlDataSource = new DataSource({
   logging: true,
   synchronize: true,
   migrations: ['src/migrations/*.ts'],
-  migrationsRun: true,
-  extra: {
-    connectTimeout: 30000
-  }
+  migrationsRun: true
 });
