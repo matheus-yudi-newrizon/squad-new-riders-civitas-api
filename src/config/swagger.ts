5d5cd4dd35029653548e87f5248fbb7b7fceb083
rgbritoamanda <amandaramires.brito@gmail.com>
2024-11-08 17:06:01 -0300

    docs(swagger): adicionar esquemas faltantes para Class e Teacher

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -26,6 +26,89 @@ export const swaggerConfig: swaggerJSDoc.OAS3Options = {
         }
       },
       schemas: {
+        Class: {
+          type: 'object',
+          properties: {
+            id: {
+              type: 'string',
+              description: 'ID da turma',
+              example: '1'
+            },
+            name: {
+              type: 'string',
+              description: 'Nome da turma',
+              example: 'Turma A'
+            },
+            schoolYear: {
+              type: 'string',
+              enum: ['1st year', '2nd year', '3rd year', '4th year', '5th year', '6th year'],
+              description: 'Ano letivo',
+              example: '1st year'
+            },
+            schoolShift: {
+              type: 'string',
+              enum: ['Morning', 'Afternoon', 'Night'],
+              description: 'Turno da turma',
+              example: 'Morning'
+            },
+            educationType: {
+              type: 'string',
+              enum: ['Nursery', 'Preschool', 'Elementary school 1'],
+              description: 'Tipo de ensino',
+              example: 'Preschool'
+            }
+          },
+          required: ['id', 'name', 'schoolYear', 'schoolShift', 'educationType']
+        },
+        Teacher: {
+          type: 'object',
+          properties: {
+            id: {
+              type: 'string',
+              description: 'ID do professor',
+              example: '1'
+            },
+            fullName: {
+              type: 'string',
+              description: 'Nome completo do professor',
+              example: 'João Silva'
+            },
+            cpf: {
+              type: 'string',
+              description: 'CPF do professor',
+              example: '123.456.789-00'
+            },
+            registrationNumber: {
+              type: 'string',
+              description: 'Número de matrícula do professor',
+              example: '20230001'
+            },
+            classes: {
+              type: 'array',
+              items: {
+                $ref: '#/components/schemas/Class'
+              },
+              description: 'Lista de turmas associadas ao professor',
+              example: [
+                {
+                  id: '1',
+                  name: 'Turma A',
+                  schoolYear: '1st year',
+                  schoolShift: 'Morning',
+                  educationType: 'Preschool'
+                },
+                {
+                  id: '2',
+                  name: 'Turma B',
+                  schoolYear: '2nd year',
+                  schoolShift: 'Afternoon',
+                  educationType: 'Elementary school 1'
+                }
+              ]
+            }
+          },
+          required: ['id', 'fullName', 'cpf', 'registrationNumber']
+        },
         CreateClassDTO: {
           type: 'object',
           properties: {
