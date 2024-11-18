54fbae0db65820190785e1dc90d6c3f5adc5830f
issaahr <isabeli.romano@gmail.com>
2024-11-18 20:59:26 -0300

    feat: adiciona controle de acesso por roles e melhorias no authMiddleware
    - Incluído middleware `roleMiddleware` para restringir acesso por roles.
    - Atualizado `authMiddleware` para adicionar `role` ao `res.locals`.
    - Adicionado controle de roles em rotas de professores, estudantes, turmas e admin.
    - Ajustada expiração do JWT para 3h.
    - Criado erro `ForbiddenError` para cenários de acesso negado.
    - Adicionada verificação de existência de corpo na requisição no validationMiddleware.
    
    Co-authored-by: rgbritoamanda <amandaramires.brito@gmail.com>

--- src/routes/StudentRoutes.ts
+++ src/routes/StudentRoutes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { roleMiddleware } from 'middlewares/roleMiddleware';
 import { Container } from 'typedi';
 import { StudentController } from '../controller/StudentController';
 import { authMiddleware } from '../middlewares/authMiddleware';
@@ -12,9 +13,12 @@ const studentController: StudentController = Container.get(StudentController);
  * @route POST /students/register
  * @description Esta rota registra um novo estudante. Requer autenticação e valida o corpo da requisição contra CreateStudentDTO.
  * @middleware authMiddleware - Garante que o usuário está autenticado.
+ * @middleware roleMiddleware['admin'] - Restringe acesso a administradores.
  * @middleware validationMiddleware(CreateStudentDTO) - Valida o corpo da requisição.
  * @access Private
  */
-studentRoutes.post('/register', authMiddleware, validationMiddleware(CreateStudentDTO), (req, res) => studentController.create(req, res));
+studentRoutes.post('/register', authMiddleware, roleMiddleware['admin'], validationMiddleware(CreateStudentDTO), (req, res) =>
+  studentController.create(req, res)
+);
 
 export default studentRoutes;
--- src/services/JwTService.ts
+++ src/services/JwTService.ts
@@ -4,7 +4,7 @@ import { Service } from 'typedi';
 @Service()
 export class JwtService {
   private static readonly secretPass: string = process.env.JWT_PASS;
-  private static readonly tokenExpiration: string = '1h';
+  private static readonly tokenExpiration: string = '3h';
 
   /**
    * Gera um token JWT com os dados do payload.
